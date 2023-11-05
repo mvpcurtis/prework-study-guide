55a73d5657090943b85d7eda014a418af2a07034
mvpcurtis <curtis.bowen@gmx.com>
2023-11-05 20:48:32 +0000

    added some more javascript to the application

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,31 @@
+
 var topics = ["HTML", "CSS", "GIT", "JavaScript"];
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-for (i = 0; i < topics.length; i++) {
-    console.log(topics[i]);
+function listTopics() {
+    for (i = 0; i < topics.length; i++) {
+        console.log(topics[i]);
+    }
 }
 
-/*
-if (topic === 'HTML') {
- console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
- console.log("Let's study CSS!");
-} else if (topic === 'Git') {
- console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
- console.log("Let's study JavaScript!");
-} else {
- console.log('Please try again!');
+// var topic = "HTML";
+
+function selectTopic() {
+    if (randomTopic === "HTML") {
+        console.log("Let's learn HTML!");
+    } else if (randomTopic === "CSS") {
+        console.log("Let's learn CSS!");
+    } else if (randomTopic === "GIT") {
+        console.log("Let's learn GIT!");
+    } else if (randomTopic === "JavaScript") {
+        console.log("Let's learn JS!");
+    } else {
+        console.log("Please try again!");
+    }
 }
-*/
+
+console.log("Here are the topics we learned through the prework:");
+listTopics();
+console.log("Which topic should we study first?");
+selectTopic();
+
